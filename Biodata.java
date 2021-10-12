89c44e2cf0dea7b4456a15526d49ad729b2fe9ec
Agistira <agiistira@gmail.com>
2021-10-12 15:54:19 +0700

    first commit

---
+++ Biodata.java
@@ -0,0 +1,26 @@
+public class Biodata {
+    public static void main(String args[]){
+        //first name, last name, age, addres, hobi, phone number, makanan kesukaan, berat badan
+        String firstName, lastName, Alamat, hobi, makanankesukaan, phoneNumber;
+        int usia; 
+        double beratBadan;
+
+        firstName = "Agistira ";
+        lastName = "Lamunde";
+        Alamat = "Tanjungpinang";
+        hobi = "Games";
+        usia = 23;
+        phoneNumber = "083183530014";
+        beratBadan = 50;
+        makanankesukaan = "Ayam Geprek";
+
+        System.out.println("Nama : " + firstName + lastName);
+        System.out.println("Alamat : " + Alamat);
+        System.out.println("Usia : " + usia);
+        System.out.println("Berat Badan : " + beratBadan + "kg");
+        System.out.println("Hobi : " + hobi);
+        System.out.println("Nomor Telepon: " + phoneNumber);
+        System.out.println("makanankesukaan : " + makanankesukaan);
+
+    }
+}    
